684d5f55868a63e6218f433a524c84c887a5295c
Matheus F Prado <matheusfenoliodoprado@outlook.com>
2020-07-09 20:35:50 -0300

    Upload_v1

---
+++ src/App.tsx
@@ -0,0 +1,165 @@
+import React from 'react';
+import GlobalStyle, { Container, Menu, Content } from './styles/global';
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
+
+const App: React.FC = () => {
+  return (<>
+    <GlobalStyle />
+    <Container>
+      <Menu>
+        <img src="https://avatars2.githubusercontent.com/u/37229507?s=460&u=f0810fe968ce8a88395bc09e41444cbba3636c7b&v=4" alt="Matheus F Prado" />
+        <a href="https://github.com/matheusfenolio" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/matheusfprado/" target="_blank">
+          <AiFillLinkedin />
+        </a>
+        <a href="mailto:mtfprado@outlook.com" target="_blank">
+          <AiOutlineMail />
+        </a>    
+      </Menu>
+      <Content>
+        <section>
+          <strong>Personal</strong>
+          <hr />
+          <ul>
+            <li>
+              <strong>Fullname: </strong>
+              <span>Matheus Fenólio do Prado</span>
+            </li>
+            <li>
+              <strong>Age: </strong>
+              <span>23</span>
+            </li>
+            <li>
+              <strong>Country: </strong>
+              <span>Brazil</span>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <strong>School</strong>
+          <hr />
+          <ul>
+            <li>
+              <strong>ETEC Dr. Carolino da Motta e Silva - </strong>
+              <span>2012 - 2015</span>
+              <ul>
+                <li>
+                  Web development
+                </li>
+                <li>
+                  Desktop development
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Unipinhal - </strong>
+              <span>2015 - 2019</span>
+              <ul>
+                <li>
+                  Computer engineering
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Rocketseat - </strong>
+              <span>2020</span>
+              <ul>
+                <li>
+                  Node.js
+                </li>
+                <li>
+                  React.js
+                </li>
+                <li>
+                  React Native
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <strong>Professional</strong>
+          <hr />
+          <ul>
+            <li>
+              <strong>Tuca Infomática - </strong>
+              <span>2012 - 2017</span>
+              <ul>
+                <li>
+                  Support
+                </li>
+                <li>
+                  Client attendance
+                </li>
+                <li>
+                  Infrasctructure and network
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Palinialves - </strong>
+              <span>2017 - 2019</span>
+              <ul>
+                <li>
+                  Operations
+                </li>
+                <li>
+                  .net Developer
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>International paper - </strong>
+              <span>01/2020 - 03/2020</span>
+              <ul>
+                <li>
+                  Operations
+                </li>
+              </ul>
+            </li>
+            <li>
+              <strong>Monitora - </strong>
+              <span>03/2020 - Now</span>
+              <ul>
+                <li>
+                  Full stack developer
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <strong>Skills</strong>
+          <hr />
+          <ul>
+            <li>
+              .net, .netCore, SpringBoot, Node.js, React, React Native and Expo
+            </li>
+            <li>
+              Kafka
+            </li>
+            <li>
+              Hibernate, Jooq, TypeOrm
+            </li>
+            <li>
+              Sql Server, Mysql, MongoDb, Oracle, Postgres 
+            </li>
+            <li>
+              Docker, Kubernetes, Jenkins, Vercel
+            </li>
+            <li>
+              Windows, Linux
+            </li>
+          </ul>
+        </section>
+      </Content>
+    </Container>
+  </>); 
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,95 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f4f4f4 ;
+    -webkit-font-smoothing: antialiased;
+
+    a {
+      color: #3a3535; /* blue colors for links too */
+      text-decoration: none; /* no underline */
+    }
+
+  }
+  body, input, button {
+    font: 16px "Poppins", sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-height: 100vh;
+    height: 100vh;
+    color: #3a3535;
+    font-family: 'Ubuntu', sans-serif;
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    background: #ff7315;
+    height: 80px;
+    padding: 8px;
+
+    img {
+        height: 100%;
+        border-radius: 50%;
+        -webkit-box-shadow: 3px 2px 5px -2px rgba(35,32,32,1);
+        -moz-box-shadow: 3px 2px 5px -2px rgba(35,32,32,1);
+        box-shadow: 3px 2px 5px -2px rgba(35,32,32,1);
+    }
+
+    svg {
+        font-size: 24px;
+        cursor: pointer;
+    }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex: 1;
+    flex-flow: row wrap;
+    padding: 24px;
+    overflow: auto;
+    text-decoration: none;
+
+    section {
+        overflow: hidden;
+        padding-left: 12px;
+
+        & + section {
+            margin-top: 32px;
+        }
+    
+        > strong {
+          font-size: 24px;
+        }
+
+        ul {
+         padding: 8px;
+        }
+
+        li { 
+          padding: 4px;
+        }
+    }
+
+    hr {
+        width: 90vw;
+        margin-bottom: 16px;
+    }
+    
+`;
